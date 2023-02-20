193cff408dd440869785f03a09ec58fad17ff1f7
madece <114481713+madece@users.noreply.github.com>
2023-02-20 16:51:18 +0100

    Update DDosMitigation.py

--- DDosMitigation.py
+++ DDosMitigation.py
@@ -2,10 +2,15 @@ import requests
 import json
 import time
 
-targetedSwitch = '00:00:00:00:00:00:00:01'
+targetedSwitch = '00:00:00:00:00:00:00:03'
 sFlow_RT = 'http://192.168.10.4:8008'
 floodlight = 'http://192.168.10.6:8080'
-defense = {'icmp': True, 'syn': False, 'dns_amplifier': False, 'udp': False}
+defense = {
+    'icmp': True,
+    'syn': False,
+    'dns_amplifier': False,
+    'udp': False
+}
 black_list = []
 block_time = 360
 fw_priority = '32767'
@@ -15,21 +20,19 @@ value = 'bytes' # set to 'bytes' and multiply 8 to get bits/second
 # define ICMP flood attack attributes #
 icmp_flood_keys = 'inputifindex,ethernetprotocol,macsource,macdestination,ipprotocol,ipsource,ipdestination'
 icmp_flood_metric_name = 'icmp_flood'
-icmp_flood_threshold_value = 50
+icmp_flood_threshold_value = 20
 icmp_flood_flows = {'keys': icmp_flood_keys, 'value': value} # No filter, the script will monitor every host
 icmp_flood_threshold = {'metric': icmp_flood_metric_name, 'value': icmp_flood_threshold_value}
 
 while True:
     r = -1
     r = requests.put(sFlow_RT + '/group/lf/json', data=json.dumps(groups))
-
     if defense['icmp']:
         # define flows and threshold of ICMP flood
-        r = requests.put(sFlow_RT + '/flow/' + icmp_flood_metric_name + '/json', data=json.dumps(icmp_flood_flows))
-        r = requests.put(sFlow_RT + '/threshold/' + icmp_flood_metric_name + '/json', data=json.dumps(icmp_flood_threshold))
-
-    event_url = sFlow_RT + '/events/json?maxEvents=10&timeout=60'
-    eventID = -1
+        r1 = requests.put(sFlow_RT + '/flow/' + icmp_flood_metric_name + '/json', data=json.dumps(icmp_flood_flows))
+        r2 = requests.put(sFlow_RT + '/threshold/' + icmp_flood_metric_name + '/json', data=json.dumps(icmp_flood_threshold))
+        event_url = sFlow_RT + '/events/json?maxEvents=10&timeout=60'
+        eventID = -1
 
     if black_list.__len__() > 0 and black_list[0][0] < time.time():
         r = requests.delete(floodlight + '/wm/staticflowentrypusher/json', data=black_list.pop(0)[1])
@@ -68,11 +71,10 @@ while True:
                                 push_data = json.dumps(message)
                                 r = requests.post(floodlight +'/wm/staticflowentrypusher/json', data=push_data)
 
-                            black_list.append([time.time()+block_time, push_data])
-                            result = r.json()
-                            print("result" + str(result))
-                            break
-                        else:
-                            continue
+                                black_list.append([time.time()+block_time, push_data])
+                                result = r.json()
+                                print("result" + str(result))
+                                break
+                            else:
+                                continue
     time.sleep(3)
-
